809fd906e2c33d98ce47cd1042c756a4b57458eb
Carlos Navarro <carlos.navarro@imesasistemas.com>
2014-08-14 14:42:59 -0600

    Tree View detail repair Editable

--- autorepair.py
+++ autorepair.py
@@ -34,10 +34,10 @@ class repair(osv.Model):
     _name='autorepair.repair'
     
     
-    def create(self, cr, uid, vals, context=None):
-        sequence=self.pool.get('ir.sequence').get(cr, uid, 'autorepair.repair')
-        vals['repair_num']=sequence
-        return super(student, self).create(cr, uid, vals, context=context)
+    #def create(self, cr, uid, vals, context=None):
+    #    sequence=self.pool.get('ircreate.sequence').get(cr, uid, 'autorepair.repair')
+    #    vals['repair_num']=sequence
+    #   return super(repair, self).create(cr, uid, vals, context=context)
 
     _columns={
               'repair_num': fields.char(string="Repair Number",required=True,readonly=True),
@@ -46,21 +46,23 @@ class repair(osv.Model):
               'in_date':fields.date(string="Check In"),
               'out_date':fields.date(string="Check Out"),
               'hours':fields.float(string="Hours",digits=(6,2)),
-              'client_id':fields.related('vehicle_id','name',readonly=True,type='many2one',relation='res.partner',string="Client"),
+              'client_id':fields.related('vehicle_id','client_id',readonly=True,type='many2one',relation='res.partner',string="Client",store=True),
               'detail_repair_ids':fields.one2many('autorepair.detail_repair','repair_id',string="Detail Repair"),
 
               }
+    _rec_name='repair_num'
     _defaults={
-        'hours':1,
         'in_date':fields.date.today,
-        'out_date':fields.date.today,
+        'out_date':False,
         'repair_num':lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid,'autorepair.repair'),
 
         }
-    def onchange_get_client(self, cr, uid, ids, vehicle,context={}):
-      for repair in self.browse(cr, uid, ids, context=context):
-          print repair.vehicle_id.client_id.id
-          return repair.vehicle_id.client_id.id
+    def onchange_vehicle_id(self, cr, uid, ids, vehicle_id,context={}):
+        data = {'client_id': False}
+        if vehicle_id:
+            vehicle = self.pool.get('autorepair.vehicle').browse(cr, uid, vehicle_id, context)
+            data.update({'client_id': vehicle.client_id.id})
+        return {'value': data}
                 
 
 
@@ -71,8 +73,9 @@ class repair(osv.Model):
         return True
     def _check_dates(self, cr, uid, ids, context={}):
         for repair in self.browse(cr, uid, ids, context=context):
-            if repair.repair_id.out_date<repair.repair_id.in_date:
-                return False
+            if repair.out_date:
+                if repair.out_date<repair.in_date:
+                    return False
         return True
 
     _constraints = [
@@ -87,40 +90,42 @@ class repair(osv.Model):
         'UNIQUE(repair_id)',
         'The repair number already exist ')
      ]
-    _order="in_date,client_id"
+    _order="in_date"
 
 class detail_repair(osv.Model):
     _name='autorepair.detail_repair'
 
+    
     _columns={
               'repair_id':fields.many2one('autorepair.repair',required=True,string="Repair Number"),
               'part_id':fields.many2one('autorepair.part',required=True,string="Part Number"),
               'units':fields.integer(string="Units",size=4),
-              'price':fields.related('part_id','price',readonly=True,type='many2one',relation='autorepair.part'),
-              'vehicle_id':fields.related('repair_id','register',readonly=True,type='many2one',relation='autorepair.vehicle'),
-              'client_id':fields.related('vehicle_id','name',readonly=True,type='many2one',relation='res.partner'),
-
+              'price':fields.float(string="Price", reandonly=True),
               }
     _defaults={
         'units':1,
         'price':0
         }
+    
+    def onchange_part_id(self, cr, uid, ids, part_id,context=None):
+        res = {}
+        if part_id:
+            part = self.pool.get('autorepair.part').browse(cr, uid, part_id, context=context)
+            res['price'] = part.price
+        return {'value': res}
 
-    def _check_unit_no_negative(self, cr, uid, ids, context={}):
-        for repair in self.browse(cr, uid, ids, context=context):
-            if repair.hours<=0:
-                return False
-        return True
 
-    _constraints = [
-        (_check_unit_no_negative,'Negatives or zero units not allowed',['unit']
-         ),
+    #def _check_unit_no_negative(self, cr, uid, ids, context={}):
+    #    for repair in self.browse(cr, uid, ids, context=context):
+    #        if repair.hours<=0:
+    #            return False
+    #    return True
+
+    #_constraints = [
+    #    (_check_unit_no_negative,'Negatives or zero units not allowed',['unit']
+    #     ),
      
-    ]
-
-    _order="repair_id,client_id"
-
-
+    #]
 
 class part(osv.Model):
     _name='autorepair.part'
@@ -135,7 +140,7 @@ class part(osv.Model):
         'price':0,
         'stock':1,
         }
-
+    _rec_name='reference'
     def _check_price_no_negative(self, cr, uid, ids, context={}):
         for part in self.browse(cr, uid, ids, context=context):
             if part.price<0:
